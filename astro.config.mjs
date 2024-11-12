32b2a85019e2a1552f9b60d0b1c9ed5ad15e4fe0
Gabriel Chiari <gabrielchiari.taf@gmail.com>
2024-11-12 10:23:44 -0500

    init

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// Import the Vercel adapter
+import vercel from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel(),
+});
